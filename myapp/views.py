b97b2fcea598bc85939b65db26e831bfc22a549b
Michael-oss138 <nwaizumichael0@gmail.com>
2025-11-09 20:04:37 +0100

    worked on the the views for the form

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import getdata
 # Create your views here.
 
-def index(requests):
+def index(request):
+    if request.method=="POSt":
+        name = request>POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        if not name or email or message:
+            message.error(request, "All fields are reqired")
+            return redirect("index")
+
+            getdata.objects.create(
+                name=name,
+                email=email,
+                subject=subject,
+                message=message
+            )
+            message.success(request, "Thank you! Your message your sent.")
+            return redirect("index")
     return render(request, 'index.html')
